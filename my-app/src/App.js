b61dd623c91132d52ec00a23d5247559823d9310
Janhavi Dahihande <janhavidahihande@gmail.com>
2019-10-25 11:47:32 -0700

    Initial commie

---
+++ my-app/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { createStore } from 'redux';
+
+function App() {
+  const counter = (state = 0, action ) => {
+    switch (action.type) {
+        case 'INCREMENT': 
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+const store = createStore( counter );
+
+store.subscribe(() => {
+  console.log("Current state: " + store.getState());
+});
+
+const dispatchStore = () => {
+  store.dispatch({ type: 'INCREMENT' })
+};
+
+return (
+  <div className="App">
+    <header className="App-header">
+      <button onClick={ dispatchStore }>Increment counter</button>
+    </header>
+  </div>
+);
+}
+
+export default App;
